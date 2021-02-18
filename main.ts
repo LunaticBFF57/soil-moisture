b9bfba70b9593651604081dd262184719c02734b
LunaticBFF57 <78350612+LunaticBFF57@users.noreply.github.com>
2021-02-19 09:13:19 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let reading = 0
+basic.forever(function () {
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    reading = pins.analogReadPin(AnalogPin.P0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+    }
+    basic.pause(5000)
+})
