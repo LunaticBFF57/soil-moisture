bafbc123688f32a1513f3a4b13e2706bc5f9201d
LunaticBFF57 <78350612+LunaticBFF57@users.noreply.github.com>
2021-03-11 20:10:53 +1100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # Config and other stuff that should not be changed.
 reading = 0
-radio.set_transmit_serial_number(True)
 radio.set_group(4)
 
 # ______________________________________________________________
 # You can change all these variables that are in between the lines.
 # Set the 'pause2' variable to set the 'sleep' time of the Micro:Bit to save power.
-pause2 = 500
+pause2 = 0
 # If no pause is desired, simply uncomment the comment, according to your respectable coding language.
 # pause2 = 0 # (for Python)
 # let pause2 = 0 // (for JavaScript)
@@ -29,11 +28,21 @@ def on_forever():
     global reading
     pins.analog_write_pin(AnalogPin.P1, 1023)
     reading = pins.analog_read_pin(AnalogPin.P0)
-    radio.send_number(reading / 4)
     pins.analog_write_pin(AnalogPin.P1, 0)
     led.plot_bar_graph(reading, 1023)
+    if reading < 750:
+        basic.show_icon(IconNames.UMBRELLA)
+        pins.servo_write_pin(AnalogPin.P2, 0)
+        basic.pause(3000)
+        pins.servo_write_pin(AnalogPin.P2, 80)
+        basic.pause(3000)
+        pins.analog_write_pin(AnalogPin.P2, 0)
     if input.button_is_pressed(Button.A):
         basic.show_number(reading)
+    '''
+    # Sends the number to the PC (must be connected to the PC via USB).
+    serial.write_value("Water Wetness", reading)
+    '''
     basic.pause(pause2)
 basic.forever(on_forever)
 
--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 //  Config and other stuff that should not be changed.
 let reading = 0
-radio.setTransmitSerialNumber(true)
 radio.setGroup(4)
 //  ______________________________________________________________
 //  You can change all these variables that are in between the lines.
 //  Set the 'pause2' variable to set the 'sleep' time of the Micro:Bit to save power.
-let pause2 = 500
+let pause2 = 0
 //  If no pause is desired, simply uncomment the comment, according to your respectable coding language.
 //  pause2 = 0 # (for Python)
 //  let pause2 = 0 // (for JavaScript)
@@ -26,12 +25,25 @@ basic.forever(function on_forever() {
     
     pins.analogWritePin(AnalogPin.P1, 1023)
     reading = pins.analogReadPin(AnalogPin.P0)
-    radio.sendNumber(reading / 4)
     pins.analogWritePin(AnalogPin.P1, 0)
     led.plotBarGraph(reading, 1023)
+    if (reading < 750) {
+        basic.showIcon(IconNames.Umbrella)
+        pins.servoWritePin(AnalogPin.P2, 0)
+        basic.pause(3000)
+        pins.servoWritePin(AnalogPin.P2, 80)
+        basic.pause(3000)
+        pins.analogWritePin(AnalogPin.P2, 0)
+    }
+    
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
     }
     
+    /** 
+    # Sends the number to the PC (must be connected to the PC via USB).
+    serial.write_value("Water Wetness", reading)
+    
+ */
     basic.pause(pause2)
 })
